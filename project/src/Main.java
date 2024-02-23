1a5a2424fe4cc337e972aa5cb4b90f6f0d361489
Fulano de Tal <gustavosantosteixeira98@gmail.com>
2024-02-23 20:36:55 -0300

    .

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,7 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.Task;
+
+
 public class Main {
     public static void main(String[] args) {
-
+        Task task = new Task();
+        task.create("Alta","Criar classes");
+        task.create("Baixa","Ligar para Camily");
+        task.print();
+        task.delete("Criar classes");
     }
 }
--- project/src/model/Task.java
+++ project/src/model/Task.java
@@ -1,17 +1,46 @@
 package model;
 
+//used libraries
+import java.util.ArrayList;
+import  java.util.List;
 public class Task {
-    public Task(){
 
+    //attributes
+
+    public List<String> tasks = new ArrayList<>();
+    public List<String>  taskcontent = new ArrayList<>();
+    public int count = 0;
+
+    public Task(){}
+
+    //method for creating tasks
+    public void create(String priority,String content){
+        taskcontent.add(content);
+        tasks.add("\n");
+        count+=1;
+        String content_task = "Task [ "+count+" ] , "+"Priority[ "+priority+" ] , "+"Content[ "+content+" ]";
+        tasks.add(content_task);
+        tasks.add("\n");
     }
 
-    public void create(){
+    //method for deleting tasks
+    public void delete(String content) {
+        int occurrences = 0;
 
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).equals(content)) {
+                occurrences++;
+            }
+        }
+
+        if (occurrences == 0) {
+            System.out.println("Dado não encontrado");
+        } else {
+            System.out.println("Total de " + occurrences + " dados deletados");
+            tasks.removeIf(task -> task.equals(content));
+        }
     }
 
-    public void delete(){
-
-    }
 
     public void search(){
 
@@ -19,6 +48,12 @@ public class Task {
 
     public void deleteAll(){
 
+    }
+
+    //method for showing tasks
+    public void print(){
+        System.out.println(tasks);
+        System.out.println(taskcontent);
     }
 
 }
